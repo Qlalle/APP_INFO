f757887d6b8cf4a5478bc965d84d9c84574a5fab
barat <guillaume.baratier@live.fr>
2015-03-25 11:26:58 +0100

    MVC
    index

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <php
+define('WEBROOT', str_remplace('index.php, '', $_SERVER['SCRIPT_NAME']));
+define('ROOT', str_remplace('index.php, '', $_SERVER['SCRIPT_FILENAME']));
 
+require(ROOT,'core/model.php');
+require(ROOT,'core/controller.php');
 
+$params = explode('/', $GET['p']);
+$controller = params[0];
+$action = isset( $params[1]) ? $params[1] : 'index';
+
+require('controllers/'.$controller.'.php');
+$controller = new $controller();
+if (methode_exists($controller, $action)){
+    $controller->$action();
+}
+else{
+echo 'erreur 404';
+}
 ?>

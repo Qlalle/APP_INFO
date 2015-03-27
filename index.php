aa048ff5842fe750a8489b71245f700df9904c38
barat <guillaume.baratier@live.fr>
2015-03-27 11:27:15 +0100

    MVC

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $action = isset( $params[1]) ? $params[1] : 'index';
 require('controllers/'.$controller.'.php');
 $controller = new $controller();
 if (method_exists($controller, $action)){
-    $controller->$controller();
+    $controller->$action();
 }
 else{
     echo 'erreur 404';

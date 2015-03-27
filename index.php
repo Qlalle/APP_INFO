d3bf223429a6a2b34f70da7eedaf44e17440dfab
barat <guillaume.baratier@live.fr>
2015-03-27 11:25:27 +0100

    Revert "MVC qui fonctionne!"
    This reverts commit 0f66b391a937bde0b8318f6efe8b631ea446865f.

--- index.php
+++ index.php
@@ -7,14 +7,12 @@ require(ROOT.'core/controller.php');
 
 $params = explode('/', $_GET['p']);
 $controller = $params[0];
-
-
 $action = isset( $params[1]) ? $params[1] : 'index';
 
-require('controllers'.DIRECTORY_SEPARATOR.$controller.'.php');
+require('controllers/'.$controller.'.php');
 $controller = new $controller();
 if (method_exists($controller, $action)){
-    $controller->$action();
+    $controller->$controller();
 }
 else{
     echo 'erreur 404';

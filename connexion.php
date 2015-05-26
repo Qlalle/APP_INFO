f8d4c2d674350ce7175792879112097f0b6d146e
barat <guillaume.baratier@live.fr>
2015-05-26 15:04:35 +0200

    répertoire connexion
    renvoie sur la page d'acceuil apres connexion

--- connexion.php
+++ connexion.php
@@ -14,8 +14,10 @@ $req->execute(array($email));
 
 if (password_verify($mdp, $password['user_password']))
 {
-  
+    session_start();
     $_session['user_email']= $email;
+    header('Location: /APP_INFO/Page_d\'accueil.php');
+    echo 'vous etes bien connecte.';
 }
 else
 {

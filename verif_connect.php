a93845aa4503f9ee6e9528f00e54471c42f9e4ce
barat <guillaume.baratier@live.fr>
2015-05-27 12:22:07 +0200

    plus d'accés aux ventes sans connexion
    seulement sur page d'acceuil.
    Sans retour +js

---
+++ verif_connect.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guillaume
+ * Date: 27/05/2015
+ * Time: 11:34
+ */
+session_start();
+if ((!isset($_SESSION['user_email'])) || ($_SESSION['user_email'] == '')){
+
+
+    $message='Vous devez d\'abord vous connecter';
+
+    echo '<script type="text/javascript">window.alert("'.$message.'");</script>';
+
+}
+else{
+    header('Location: /APP_INFO/Page_vente.php');
+}
+?>

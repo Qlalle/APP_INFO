f74a7327bc533020a3b7bbc1c1396406fc2059ab
Maxime Legallois <mlegallois@gmail.com>
2015-05-27 09:27:27 +0200

    cadre membre
    petit espace après bonjour

--- cadre_membre.php
+++ cadre_membre.php
@@ -5,9 +5,9 @@
  * Date: 26/05/2015
  * Time: 16:25
  */
- echo "Bonjour" .$_SESSION['user_email'];
+ echo "Bonjour " .$_SESSION['user_email'];
 ?>
-<!DOCTYPE html>
+<DOCTYPE html>
 <html>
 <p id="boutonDeco">
     <input type="submit" value="Déconnexion" />

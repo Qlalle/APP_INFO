e025327dea56a8c53dba7b68f35556c32b707596
barat <guillaume.baratier@live.fr>
2015-06-01 10:58:27 +0200

    pop up!

--- verif_connect.php
+++ verif_connect.php
@@ -11,10 +11,11 @@ if ((!isset($_SESSION['user_email'])) || ($_SESSION['user_email'] == '')){
 
     $message='Vous devez d\'abord vous connecter';
 
-    echo '<script type="text/javascript">window.alert("'.$message.'");</script>';
+    echo '<script type="text/javascript">window.alert("'.$message.'"); window.location.href="Page_accueil.php";</script>';
 
 }
 else{
     header('Location: /APP_INFO/Page_vente.php');
+
 }
 ?>
